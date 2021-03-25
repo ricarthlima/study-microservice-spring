7de9974d29c39405c96264be3c6b326154c1fa68
Ricarth Lima <rrsl@cin.ufpe.br>
2021-03-24 23:33:07 -0300

    initial commit | ms-programmer initialized

---
+++ ms-programmer/src/main/java/com/lima/ricarth/msprogrammer/MsProgrammerApplication.java
@@ -0,0 +1,13 @@
+package com.lima.ricarth.msprogrammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProgrammerApplication.class, args);
+	}
+
+}
---
+++ ms-programmer/src/test/java/com/lima/ricarth/msprogrammer/MsProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lima.ricarth.msprogrammer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
